b94de4a9251d5e42b9ef47760c6775d67f6b4cbe
Diego Suazo <di.suazov@duocuc.cl>
2024-10-08 16:04:12 -0300

    08-10-2024 version 0.26

--- js/funciones.js
+++ js/funciones.js
@@ -38,13 +38,44 @@ function copiarNumero(){
     document.getElementById("contacto").innerHTML = texto + " " + condicion;
 }
 
-function descargarCV() {
-    const texto = "CV_Diego_Suazo";
+// function descargarCV() {
+//     const texto = "Currículum Diego Suazo";
+//     const condicion = "[Descargado]";
+//     const link = document.createElement('a');
+//     navigator.clipboard.writeText(texto);
+//     link.href = 'doc/Currículum Diego Suazo.pdf';  
+//     link.download = 'Currículum Diego Suazo.pdf';
+//     document.body.appendChild(link);
+//     link.click();
+//     document.body.removeChild(link);
+//     document.getElementById("contacto").innerHTML = texto + " " + condicion;
+// }
+
+function descargar(archivo) {
+    let texto = "";
     const condicion = "[Descargado]";
     const link = document.createElement('a');
+    switch(archivo){
+        case toeic:
+            texto = "TOEIC Bridge";
+            link.href = 'doc/TOEIC.pdf';  
+            link.download = 'TOEIC Bridge.pdf';
+            break;
+        case scrum:
+            texto = "Certificado Scrum";
+            link.href = 'doc/Certificate Scrum.pdf';  
+            link.download = 'Certificado Scrum.pdf';
+            break;
+        case curriculum:
+            texto = "Currículum Diego Suazo";
+            link.href = 'doc/Currículum Diego Suazo.pdf';  
+            link.download = 'Currículum Diego Suazo.pdf';
+            break;
+        default:
+        console.error('Archivo no reconocido');
+        return;
+    }
     navigator.clipboard.writeText(texto);
-    link.href = 'doc/Currículum Diego Suazo.pdf';  
-    link.download = 'CV_Diego_Suazo.pdf';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
